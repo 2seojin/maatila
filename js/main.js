829ab6d96cbd8c8f85261628f443359394d820a3
2seojin <95662420+2seojin@users.noreply.github.com>
2021-12-30 13:03:46 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -70,4 +70,50 @@ $(document).ready(function(){
             disableOnInteraction: false
         }
     });
+    //윈도우에 스크롤 이벤트 설정
+    $(window).scroll(function(){
+        //브라우저에서 html문서의 scrollTop값을 인식하여 winTop변수에 저장
+        //$(this)는 scroll이벤트가 발생한 객체(window)를 가리킴
+        var winTop=$(this).scrollTop();
+        console.log(winTop);
+        //offset().top => scrollTop()으로부터 떨어져 있는 거리
+        //s2영역의 위쪽 위치값을 구해서 scroll1변수에 저장
+        var scroll1=$('section.s2').offset().top-600;
+        //s3영역의 위쪽 위치값을 구해서 scroll2변수에 저장
+        var scroll2=$('section.s3').offset().top-600; 
+        //s4영역의 위쪽 위치값을 구해서 scroll3변수에 저장
+        var scroll3=$('section.s4').offset().top-600; 
+        //s5영역의 위쪽 위치값을 구해서 scroll4변수에 저장
+        var scroll4=$('section.s5').offset().top-600; 
+        //s6영역의 위쪽 위치값을 구해서 scroll5변수에 저장
+        var scroll5=$('section.s6').offset().top-600; 
+
+        //만약 .s2영역이 보이면, && ('그리고' 라는 뜻의 논리 연산자)조건 2개 모두 true일 때 실행
+        if(scroll1 < winTop && winTop < scroll2){
+            $('section.s2 h1').addClass('active');
+            $('section.s2 p').addClass('active');
+            $('section.s2 .swiper').addClass('active');
+        }
+        //만약 .s3영역이 보이면, && ('그리고' 라는 뜻의 논리 연산자)조건 2개 모두 true일 때 실행
+        if(scroll2 < winTop && winTop < scroll3){
+            $('section.s3 h1').addClass('active');
+            $('section.s3 p').addClass('active');
+            $('section.s3 .tab').addClass('active');
+            $('section.s3 .view_btn').addClass('active');
+        }
+        //만약 .s4영역이 보이면, && ('그리고' 라는 뜻의 논리 연산자)조건 2개 모두 true일 때 실행
+        if(scroll3 < winTop && winTop < scroll4){
+            $('section.s4 h1').addClass('active');
+            $('section.s4 p').addClass('active');
+            $('section.s4 .gallery').addClass('active');
+            $('section.s4 .s4_right').addClass('active');
+        }
+        //만약 .s5영역이 보이면, && ('그리고' 라는 뜻의 논리 연산자)조건 2개 모두 true일 때 실행
+        if(scroll4 < winTop){
+            $('section.s5 h1').addClass('active');
+            $('section.s5 p').addClass('active');
+            $('section.s5 .vlog').addClass('active');
+        }
+        
+    });
 });
